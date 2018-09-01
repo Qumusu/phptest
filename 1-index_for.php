2c1218dc5aeb3ad14d038522a4c6ceaed88c02f1
Qumusu <kei2ano333@gmail.com>
2018-09-01 15:17:39 +0900

    for and foreach

---
+++ 1-index_for.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+for($i = 0;$i < 10 ;$i++){
+    echo $i."<br>";
+}
+*/
+
+for($i = 2;$i <= 30 ;$i++){
+    echo $i.",";
+}
+
+echo "<br><br>";
+
+for($i = 10;$i >= 0 ;$i--){
+    if($i == 0){
+      echo $i;
+    } else{
+        echo $i.",";
+    }
+}
+echo "<br><br>";
+
+$family = array("Rob","Kirsten","Tommy","Ralpie");
+
+for($i = 0; $i < sizeof($family);$i++){
+    echo $family[$i].",";
+}
+
+echo "<br><br>";
+foreach($family as $key => $value){
+        $family[$key] = $value." Percival";
+        echo $family[$key].", ";
+}
+
+
+?>
